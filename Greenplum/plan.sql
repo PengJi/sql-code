24c8b9384dd59309f826c07e1a020ae1465ba249
pengji <jipeng92@gmail.com>
2017-03-31 20:08:16 +0800

    #1 add plan.sql

--- Greenplum/plan.sql
+++ Greenplum/plan.sql
@@ -15,7 +15,7 @@ insert into right_table values (5);
 insert into right_table values (6);
 insert into right_table values (7);
 
-explain  select * from left_table as l ,right_table as r where l.tid1=r.tid2;    
+explain select * from left_table as l ,right_table as r where l.tid1=r.tid2;    
 explain select * from left_table as l ,right_table as r where l.tid1=r.tid2 and l.tid1>3;
 
 explain select * from left_table as l join right_table as r on l.tid1=r.tid2;
