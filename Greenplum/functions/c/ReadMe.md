84d0b51caa08c52c3f887a013b3002df84613260
pengji <jipeng92@gmail.com>
2017-11-24 10:27:14 +0800

    add .gitignore v1.cpp

--- Greenplum/functions/c/ReadMe.md
+++ Greenplum/functions/c/ReadMe.md
@@ -1,1 +1,3 @@
 自定义函数c实现
+
+[扩展SQL](http://www.postgres.cn/docs/9.4/xfunc-c.html)
---
+++ Greenplum/functions/c/v1.cpp
@@ -0,0 +1,94 @@
+#include "postgres.h"
+#include <string.h>
+#include "fmgr.h"
+#include "utils/geo_decls.h"
+
+/*
+ * http://www.postgres.cn/docs/9.4/xfunc-c.html
+ * 版本1调用约定
+ */
+
+#ifdef PG_MODULE_MAGIC
+PG_MODULE_MAGIC;
+#endif
+
+/*传递数值*/
+PG_FUNCTION_INFO_V1(add_one);
+
+Datum
+add_one(PG_FUNCTION_ARGS)
+{
+    int32   arg = PG_GETARG_INT32(0);
+
+    PG_RETURN_INT32(arg + 1);
+}
+
+/*传递引用，定长*/ 
+PG_FUNCTION_INFO_V1(add_one_float8);
+
+Datum
+add_one_float8(PG_FUNCTION_ARGS)
+{
+ /*用于FLOAT8的宏，隐藏其传递引用的本质。*/
+    float8   arg = PG_GETARG_FLOAT8(0);
+
+    PG_RETURN_FLOAT8(arg + 1.0);
+}
+
+PG_FUNCTION_INFO_V1(makepoint);
+
+Datum
+makepoint(PG_FUNCTION_ARGS)
+{
+ /* 这里，我们没有隐藏Point的传递引用的本质*/
+    Point     *pointx = PG_GETARG_POINT_P(0);
+    Point     *pointy = PG_GETARG_POINT_P(1);
+    Point     *new_point = (Point *) palloc(sizeof(Point));
+
+    new_point->x = pointx->x;
+    new_point->y = pointy->y;
+
+    PG_RETURN_POINT_P(new_point);
+}
+
+/*传递引用，变长*/
+PG_FUNCTION_INFO_V1(copytext);
+
+Datum
+copytext(PG_FUNCTION_ARGS)
+{
+    text     *t = PG_GETARG_TEXT_P(0);
+
+    /*
+     * VARSIZE是结构以字节计的总长度。
+     */
+    text     *new_t = (text *) palloc(VARSIZE(t));
+    SET_VARSIZE(new_t, VARSIZE(t));
+
+    /*
+     * VARDATA是结构中指向数据区的一个指针。
+     */
+    
+    memcpy((void *) VARDATA(new_t), /* 目的*/
+           (void *) VARDATA(t),     /* 源 */
+           VARSIZE(t) - VARHDRSZ);  /* 多少字节 */
+    PG_RETURN_TEXT_P(new_t);
+}
+
+PG_FUNCTION_INFO_V1(concat_text);
+
+Datum
+concat_text(PG_FUNCTION_ARGS)
+{
+    text  *arg1 = PG_GETARG_TEXT_P(0);
+    text  *arg2 = PG_GETARG_TEXT_P(1);
+    int32 new_text_size = VARSIZE(arg1) + VARSIZE(arg2) - VARHDRSZ;
+    text *new_text = (text *) palloc(new_text_size);
+
+    SET_VARSIZE(new_text, new_text_size);
+    memcpy(VARDATA(new_text), VARDATA(arg1), VARSIZE(arg1) - VARHDRSZ);
+    memcpy(VARDATA(new_text) + (VARSIZE(arg1) - VARHDRSZ),
+           VARDATA(arg2), VARSIZE(arg2) - VARHDRSZ);
+    PG_RETURN_TEXT_P(new_text);
+}
+
