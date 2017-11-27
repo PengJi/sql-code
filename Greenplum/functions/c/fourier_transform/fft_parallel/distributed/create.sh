3eabbd123a27034c3ad46f4c351013e4d45e707a
pengji <jipeng92@gmail.com>
2017-11-27 12:26:35 +0800

    add pulse.py

--- Greenplum/functions/c/fourier_transform/fft_parallel/distributed/create.sh
+++ Greenplum/functions/c/fourier_transform/fft_parallel/distributed/create.sh
@@ -1,13 +1,16 @@
+#-------------------------
+#在数据库中创建自定义函数脚本
+
+#------------------------
+
+
 make clean;
 make;
-'''
+
 scp fft.so gpdba@node1:/home/gpdba/sql_prac/Greenplum/functions/c/fourier_transform/fft_parallel/distributed/;
 scp fft.so gpdba@node2:/home/gpdba/sql_prac/Greenplum/functions/c/fourier_transform/fft_parallel/distributed/;
 scp fft.so gpdba@node3:/home/gpdba/sql_prac/Greenplum/functions/c/fourier_transform/fft_parallel/distributed/;
 scp fft.so gpdba@node5:/home/gpdba/sql_prac/Greenplum/functions/c/fourier_transform/fft_parallel/distributed/;
-'''
-
-scp fft.so gpdba@node1:/home/gpdba/sql_prac/Greenplum/functions/c/fourier_transform/fft_parallel/distributed/;
 
 psql -d testDB -f fft.sql.in
 
