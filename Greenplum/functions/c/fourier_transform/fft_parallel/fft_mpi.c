c5f156d058e896caa9f906e96fec0886f882593b
pengji <jipeng92@gmail.com>
2018-01-23 11:26:32 +0800

    modify fft_mpi.c

--- Greenplum/functions/c/fourier_transform/fft_parallel/fft_mpi.c
+++ Greenplum/functions/c/fourier_transform/fft_parallel/fft_mpi.c
@@ -3,10 +3,17 @@
 #include <math.h>
 #include "mpi.h"
 
-/*
- * Linux下快速傅立叶变换的并行求解
- * https://wenku.baidu.com/view/84f640232f60ddccda38a0a2.html
- */
+#include "postgres.h"
+#include "executor/executor.h"  /* for GetAttributeByName() */
+#include "funcapi.h"
+#include "fft.h"
+#include "access/heapam.h"
+#include "access/relscan.h"
+#include "utils/fmgroids.h"
+#include "utils/tqual.h"
+#include "executor/spi.h"
+
+PG_MODULE_MAGIC;
 
 #define MAX_N 4096
 #define PI 3.1415926535897932
@@ -28,7 +35,7 @@ typedef struct
 
 complex_t p[MAX_N],s[2*MAX_N],r[2*MAX_N];
 complex_t w[2*MAX_N];
-int variableNum;
+uint64 variableNum;
 double transTime=0,totalTime=0,beginTime;
 MPI_Status status;
 
@@ -183,6 +190,42 @@ void print(const complex_t* f, int fLength)
     printf("\n");
 }
 
+void print_ereport(const complex_t* f, int fLength)
+{
+    BOOL isPrint = FALSE;
+    int i;
+
+    /* f[0] */
+    if (abs(f[0].r) > EPS)
+    {
+        ereport(INFO,(errmsg("%f", f[0].r)));
+        isPrint = TRUE;
+    }
+
+    for(i = 1; i < fLength; i ++)
+    {
+        if (f[i].r > EPS)
+        {
+            if (isPrint)
+            	ereport(INFO,(errmsg(" + ")));
+            else
+                isPrint = TRUE;
+            ereport(INFO,(errmsg("%ft^%d", f[i].r, i)));
+        }
+        else if (f[i].r < - EPS)
+        {
+            if(isPrint)
+            	ereport(INFO,(errmsg(" - ")));
+            else
+                isPrint = TRUE;
+            ereport(INFO,(errmsg("%ft^%d", -f[i].r, i)));
+        }
+    }
+    if (isPrint == FALSE)
+    	ereport(INFO,(errmsg("0")));
+    ereport(INFO,(errmsg("\n")));
+}
+
 /*
  * Function:    myprint
  * Description: 完整打印复数数组元素，包括实部和虚部
@@ -264,6 +307,65 @@ BOOL readFromFile()
 }
 
 /*
+ * 从数据库表中读入数据
+ */
+BOOL readFromDB()
+{
+	int i;
+    char *command="select val from test order by id";
+    int ret,cnt;
+    uint64 proc;
+    float r;
+
+    //command = text_to_cstring(PG_GETARG_TEXT_P(0));
+    cnt = PG_GETARG_INT32(0);
+
+    SPI_connect();
+    ret = SPI_exec(command, cnt);
+    variableNum=SPI_processed;
+    proc = SPI_processed;
+
+	if((variableNum<1)||(variableNum>MAX_N))
+	{
+		ereport(INFO,(errmsg("variableNum out of range! \n")));
+		return(FALSE);
+	}
+	ereport(INFO,(errmsg("variableNum=%d\n",variableNum)));
+	ereport(INFO,(errmsg("\n")));
+
+    if (ret > 0 && SPI_tuptable != NULL){
+        TupleDesc tupdesc = SPI_tuptable->tupdesc;
+        SPITupleTable *tuptable = SPI_tuptable;
+        char buf[10];
+        uint64 j;
+
+        for (j = 0; j < proc; j++) //proc为表的行数
+        {
+            HeapTuple tuple = tuptable->vals[j];
+
+            for (i = 1, buf[0] = 0; i <= tupdesc->natts; i++){
+                snprintf(buf + strlen (buf), sizeof(buf) - strlen(buf), " %s%s",
+                        SPI_getvalue(tuple, tupdesc, i),
+                        (i == tupdesc->natts) ? " " : " |");
+            }
+
+            ereport(INFO,(errmsg("ROW: %s",buf))); //输出一行数据
+            sscanf(buf,"%f",&r);
+			//准备数据
+            p[j].real = r;
+            p[j].imag = 0.0f;
+        }
+    }
+
+    printf("p(t)=");
+	print(p,variableNum);
+
+	SPI_finish();
+
+	return(TRUE);
+}
+
+/*
  * Function:    sendOrigData
  * Description: 把原始数据发送给其它进程
  * Parameters:  size为集群中进程的数目
@@ -279,7 +381,7 @@ void sendOrigData(int size)
 	}
 
 }
- 
+
 /*
  * Function:    recvOrigData
  * Description:	接受原始数据
@@ -288,6 +390,122 @@ void recvOrigData()
 {
 	MPI_Recv(&variableNum,1,MPI_INT,0,V_TAG,MPI_COMM_WORLD,&status);
 	MPI_Recv(p, variableNum * 2, MPI_DOUBLE, 0, P_TAG, MPI_COMM_WORLD, &status);
+}
+
+/*
+ * Function:    recvOrigData
+ * Description:	接受原始数据
+ */
+void recvOrigData()
+{
+	MPI_Recv(&variableNum,1,MPI_INT,0,V_TAG,MPI_COMM_WORLD,&status);
+	MPI_Recv(p, variableNum * 2, MPI_DOUBLE, 0, P_TAG, MPI_COMM_WORLD, &status);
+}
+
+/*
+ * fft的udf实现
+ */
+PG_FUNCTION_INFO_V1(fft_main);
+Datum
+fft_main(PG_FUNCTION_ARGS)
+{
+	int rank,size,i;
+
+	MPI_Init(&argc,&argv);
+	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Comm_size(MPI_COMM_WORLD,&size);
+
+	if(rank==0)
+	{
+		// 0# 进程从文件读入多项式p的阶数和系数序列
+		if(!readFromDB())
+			exit(-1);
+
+		// 进程数目太多，造成每个进程平均分配不到一个元素，异常退出
+		if(size>2*variableNum)
+		{
+			ereport(INFO,(errmsg("Too many Processors,reduce your -np value \n")));
+			MPI_Abort(MPI_COMM_WORLD,1);
+		}
+
+		beginTime=MPI_Wtime();
+
+		// 0#进程把多项式的阶数,p发送给其它进程
+		sendOrigData(size);
+
+		// 累计传输时间
+		addTransTime(MPI_Wtime()-beginTime);
+
+	}
+	else // 其它进程接收进程0发送来的数据，包括variableNum、数组p
+	{
+		recvOrigData();
+	}
+
+	// 初始化数组w，用于进行傅立叶变换
+	int wLength=2*variableNum;
+	for(i=0;i<wLength;i++)
+	{
+		w[i].r=cos(i*2*PI/wLength);
+		w[i].i=sin(i*2*PI/wLength);
+	}
+
+	// 划分各个进程的工作范围 startPos ~ stopPos
+	int everageLength=wLength/size;
+	int moreLength=wLength%size;
+	int startPos=moreLength+rank*everageLength;
+	int stopPos=startPos+everageLength-1;
+
+	if(rank==0)
+	{
+		startPos=0;
+		stopPos=moreLength+everageLength-1;
+	}
+
+    // 对p作FFT，输出序列为s，每个进程仅负责计算出序列中位置为startPos 到 stopPos的元素
+	evaluate(p,0,variableNum-1,w,s,startPos,stopPos,wLength);
+	
+	ereport(INFO,(errmsg("partial results, process %d.\n",rank)));
+	myprint(s,wLength);
+	
+	// 各个进程都把s中自己负责计算出来的部分发送给进程0，并从进程0接收汇总的s
+	if(rank>0)
+	{
+		MPI_Send(s+startPos,everageLength*2,MPI_DOUBLE,0,S_TAG,MPI_COMM_WORLD);
+		MPI_Recv(s,wLength*2,MPI_DOUBLE,0,S_TAG2,MPI_COMM_WORLD,&status);
+	}
+	else // 进程0接收s片断，向其余进程发送完整的s
+	{
+		double tempTime=MPI_Wtime();
+
+		for(i=1;i<size;i++)
+		{
+			MPI_Recv(s+moreLength+i*everageLength,everageLength*2,MPI_DOUBLE,i,S_TAG,MPI_COMM_WORLD,&status);
+		}
+
+		for(i=1;i<size;i++)
+		{
+			MPI_Send(s,wLength*2,MPI_DOUBLE,i,S_TAG2,MPI_COMM_WORLD);
+		}
+
+		ereport(INFO,(errmsg("The final results :\n")));
+		printres(s,wLength);
+
+		addTransTime(MPI_Wtime()-tempTime);
+	}
+
+	if(rank==0)
+	{
+		totalTime=MPI_Wtime();
+		totalTime-=beginTime;
+
+		ereport(INFO,(errmsg("\nUse prossor size=%d\n",size)));
+		ereport(INFO,(errmsg("Total running time=%f(s)\n",totalTime)));
+		ereport(INFO,(errmsg("Distribute data time = %f(s)\n",transTime)));
+		ereport(INFO,(errmsg("Parallel compute time = %f(s)\n ",totalTime-transTime)));
+	}
+
+	MPI_Finalize();
 }
 
 int main(int argc,char * argv[])
