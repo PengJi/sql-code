10aa88c62bd82811c19dae17ad0bf1cd717b186a
pengji <jipeng92@gmail.com>
2018-03-22 22:13:07 +0800

    modify gpadmin to gpdba

--- Greenplum/functions/c/rg_test/create.sh
+++ Greenplum/functions/c/rg_test/create.sh
@@ -6,10 +6,10 @@
 make clean;
 make;
 
-scp rg.so gpadmin@node1:/home/gpadmin/sql_prac/Greenplum/functions/c/rg_test;
-scp rg.so gpadmin@node2:/home/gpadmin/sql_prac/Greenplum/functions/c/rg_test;
-scp rg.so gpadmin@node3:/home/gpadmin/sql_prac/Greenplum/functions/c/rg_test;
-scp rg.so gpadmin@node5:/home/gpadmin/sql_prac/Greenplum/functions/c/rg_test;
+scp rg.so gpdba@node1:/home/gpdba/sql_prac/Greenplum/functions/c/rg_test;
+scp rg.so gpdba@node2:/home/gpdba/sql_prac/Greenplum/functions/c/rg_test;
+scp rg.so gpdba@node3:/home/gpdba/sql_prac/Greenplum/functions/c/rg_test;
+scp rg.so gpdba@node5:/home/gpdba/sql_prac/Greenplum/functions/c/rg_test;
 
 psql -d testDB -f rg.sql.in
 
