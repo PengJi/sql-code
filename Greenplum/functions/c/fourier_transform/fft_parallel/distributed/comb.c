758eeb6df1f93cd5cd34de832d339b6ab0c0dd05
pengji <jipeng92@gmail.com>
2018-05-06 11:31:46 +0800

    modify Makefile comb.c

--- Greenplum/functions/c/fourier_transform/fft_parallel/distributed/comb.c
+++ Greenplum/functions/c/fourier_transform/fft_parallel/distributed/comb.c
@@ -500,7 +500,7 @@ int move_row(int from_segid, int to_segid, int row_id){
 	strcat(comd, dict[from_segid]);
 	strcat(comd, " -c delete from test2 where id=");
 	sscanf(row_id, "%s");
-	strcat(comd, );
+	strcat(comd, row_id);
 	memset(buff,0,sizeof(buff));
 	if((fstream=popen(comd,"r")) == NULL){
 		fprintf(stderr,"execute command failed: %s",strerror(errno));
